a838870e3f44186e486b6aa69f25edd119030790
Tongki Ilagan <tongki_ilagan@proton.me>
2025-06-03 17:23:32 +0800

    Restructured css/js files to exist into /static folder, which is to be served by tower-http::ServeDir

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use axum::{
     Router,
     Extension
 };
+use tower_http::services::ServeDir;
+
 mod routes;
 mod middlewares;
 use crate::routes::routes::routes;
@@ -22,7 +24,8 @@ async fn main() {
     let state = Arc::new(AppState { h });
 
     let app = Router::from(routes())
-                .layer(Extension(state));
+                .layer(Extension(state))
+                .nest_service("/static", ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
