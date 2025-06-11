a3d596ea791ecbffd77105394df9bfe559db1a5a
Tongki Ilagan <tongki_ilagan@proton.me>
2025-06-11 23:45:43 +0800

    Replaced handlebars crate with Askama to support compiled templating

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #![allow(non_camel_case_types)]
-use std::sync::Arc;
 use axum::{
     Router,
     Extension
@@ -10,21 +9,10 @@ mod routes;
 mod middlewares;
 use crate::routes::routes::routes;
 
-pub struct AppState {
-    h: handlebars::Handlebars<'static>,
-}
-pub type ExtAppState = Arc<AppState>;
 #[tokio::main]
 async fn main() {
-    let mut h = handlebars::Handlebars::new();
-    h.register_template_file("index", "templates/index.hbs")
-        .expect("Unable to register index template");
-    h.register_template_file("404", "templates/404.hbs")
-        .expect("Unable to register 404 template");
-    let state = Arc::new(AppState { h });
 
     let app = Router::from(routes())
-                .layer(Extension(state))
                 .nest_service("/static", ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -1,7 +1,20 @@
-use axum::{Router, routing::get, Extension, response::Html, middleware::{from_fn}};
-use crate::{ExtAppState};
+use axum::{Router, routing::get, Extension, response::{Html, IntoResponse}, middleware::{from_fn}, http::{StatusCode}};
 use serde::{Serialize};
 use serde_json::json;
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> {
+    name: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "404.html")]
+struct NotFoundTemplate<'a> {
+    header: &'a str,
+    message: &'a str,
+}
 
 use crate::middlewares::index_middleware::index_middleware;
 #[derive(Serialize)]
@@ -14,17 +27,18 @@ pub fn routes() -> Router {
         .route("/", get(root).route_layer(from_fn(index_middleware)))
         .fallback(not_found)
 }
-async fn root(Extension(state): Extension<ExtAppState>) -> Html<String>{
-    let html = state.h.render("index", &json!({})).unwrap();
-    Html(html)
-}
-async fn not_found(Extension(state): Extension<ExtAppState>) -> Html<String> {
-    let res = not_found_msg {
-        header: String::from("404"),
-        message: String::from("Page cannot be Found")
+async fn root() -> impl IntoResponse{
+    let template = IndexTemplate {
+        name: "Test"
     };
-    // optional: serialize into json
-    // let json = serde_json::to_string(&res).expect("Failed to not_found mesage convert to JSON");
-    let html = state.h.render("404", &res).unwrap();
-    Html(html)
+    //let html = state.h.render("index", &json!({})).unwrap();
+    (StatusCode::OK, Html(template.render().unwrap()).into_response())
+
+}
+async fn not_found() -> impl IntoResponse {
+    let template = NotFoundTemplate {
+        header: "404",
+        message: "Page cannot be Found"
+    };
+    (StatusCode::OK, Html(template.render().unwrap()).into_response())
 }
